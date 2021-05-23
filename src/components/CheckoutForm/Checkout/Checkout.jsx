75ccc2f2453dd89d9413d7709f08edd48277127c
fogset <zhangth1990@hotmail.com>
2021-05-23 04:34:14 -0700

    addressForm

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -8,7 +8,7 @@ import PaymentForm from '../PaymentForm';
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
-    const [activeStep, setActiveStep] = useState(2);
+    const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
 
     const Confirmation = () => (
@@ -28,7 +28,7 @@ const Checkout = () => {
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography variant="h4" align="center"> Checkout</Typography>
-                    <Stepper activeStep={0} className={classes.stepper}>
+                    <Stepper activeStep={1} className={classes.stepper}>
 
                         {steps.map((label) => (
                             <Step key={label}>

8d802927cce67f24e291c4d4e5123d20d5f5658c
fogset <zhangth1990@hotmail.com>
2021-05-24 18:31:31 -0700

    added stripe payment and the confirmation page

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import { Link, useHistory } from 'react-router-dom';
 
 import { commerce } from '../../../lib/commerce';
-
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
-import { CategoryRounded } from '@material-ui/icons';
 
 const steps = ['Shipping address', 'Payment details'];
 
-const Checkout = ({ cart }) => {
+const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
     const [shippingData, setShippingData] = useState([]);
@@ -24,7 +23,7 @@ const Checkout = ({ cart }) => {
 
                 setCheckoutToken(token);
             } catch (error) {
-
+                console.log(error)
             }
         }
 
@@ -39,15 +38,40 @@ const Checkout = ({ cart }) => {
         nextStep();
     }
 
-    const Confirmation = () => (
-        <div>
-            Confirmation
+    let Confirmation = () => (order.customer ? (
+        <>
+            <div>
+                <Typography variant="h5">Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}!</Typography>
+                <Divider className={classes.divider} />
+                <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
+            </div>
+            <br />
+            <Button component={Link} to="/" variant="outlined" type="button" >Back to home</Button>
+        </>
+    ) : (
+        <div className={classes.spinner}>
+            <CircularProgress />
         </div>
-    )
+    ));
+
+    if (error) {
+        Confirmation = () => (
+            <>
+                <Typography variant="h5">Error: {error}</Typography>
+                <br />
+                <Button component={Link} variant="outlined" type="button" to="/">Back to home</Button>
+            </>
+        );
+    }
 
     const Form = () => activeStep === 0
         ? <AddressForm checkoutToken={checkoutToken} next={next} />
-        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
+        : <PaymentForm checkoutToken={checkoutToken}
+            nextStep={nextStep}
+            backStep={backStep}
+            shippingData={shippingData}
+            onCaptureCheckout={onCaptureCheckout} />;
+
 
 
     return (

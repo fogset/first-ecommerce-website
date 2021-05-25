e24423ca9a0aac5d717e2763b09722604b5c5eb4
fogset <zhangth1990@hotmail.com>
2021-05-24 17:11:06 -0700

    added review to the PaymentForm

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
-
+import { Link } from 'react-router-dom';
 import { commerce } from '../../lib/commerce';
 
 import FormInput from './CustomTextField';
 
-const AddressForm = ({ checkoutToken }) => {
+const AddressForm = ({ checkoutToken, next }) => {
     const [shippingCountries, setShippingCountries] = useState([]);
     const [shippingCountry, setShippingCountry] = useState('');
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -61,15 +61,15 @@ const AddressForm = ({ checkoutToken }) => {
         <div>
             <Typography variant="h6" gutterBottom>Shipping Address</Typography>
             <FormProvider {...methods}>
-                <form onSubmit=' '>
+                <form onSubmit={methods.handleSubmit((data) => next({ ...data, shippingCountry, shippingSubdivision, shippingOption }))}>
                     <Grid container spacing={3}>
+
                         <FormInput name='firstName' label='First name' />
                         <FormInput name='lastName' label='Last name' />
                         <FormInput name='address1' label='Address' />
                         <FormInput name='email' label='Email' />
                         <FormInput name='city' label='City' />
                         <FormInput name='zip' label='ZIP /Postal code' />
-
                         <Grid item xs={12} sm={6}>
                             <InputLabel> Shipping Country</InputLabel>
                             <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
@@ -90,12 +90,17 @@ const AddressForm = ({ checkoutToken }) => {
 
                         <Grid item xs={12} sm={6}>
                             <InputLabel> Shipping Options</InputLabel>
-                            <Select value={shippingOption} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
+                            <Select value={shippingOptions} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
                                 {options.map((option) => (
                                     <MenuItem key={option.id} value={option.id}> {option.label}</MenuItem>
                                 ))}
                             </Select>
                         </Grid>
+                        <br />
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Button component={Link} to="/cart" variant="outlined">Back to Cart </Button>
+                            <Button type="submit" variant="contained" color="primary"> Next</Button>
+                        </div>
 
                     </Grid>
                 </form>
